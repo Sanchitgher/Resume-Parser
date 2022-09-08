e593e90c66a686e94168cc3c6806009f23fd102e
sanchit <snchtgher@gmail.com>
2022-09-08 03:54:42 +0100

    js

--- App/app.py
+++ App/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, Response, request
 import json
-from NamedEntity import *
+import spacy
+import re
+#from NamedEntity import *
 import flask
 #from tika import parser
 from werkzeug.utils import secure_filename
@@ -15,6 +17,30 @@ def pdf_to_text(filepath):
   for page in doc:
     text = text + str(page.get_text())
   return text
+
+class NamedEntityService(object):
+    model = None  # Where we keep the model when it's loaded
+    
+    @classmethod
+    def get_model(cls):
+        """Get the model object for this instance, loading it if it's not already loaded."""
+        if cls.model is None:
+            cls.model = spacy.load('My_trainedmodel_61')
+        return cls.model
+
+    
+    @classmethod
+    def get_entities(cls, input):
+        """For the input, get entities and return them."""
+        clf = cls.get_model()
+        answer_dict = dict()
+        for x in clf(input).ents:
+            label_name = x.label_
+            text_name = re.sub('[^A-Za-z0-9]+', ' ', str(x)).strip()
+            answer_dict[text_name] = label_name
+
+        # return dict([(str(x), x.label_) for x in clf(input).ents])
+        return answer_dict
 
 def preprocess(text):
   text = "".join([s for s in text.splitlines(True) if s.strip("\r\n")])
